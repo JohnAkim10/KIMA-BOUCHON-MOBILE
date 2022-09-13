0af615bfd39898a54ebd5ad248a60b0e9d9c4def
JohnAkim10 <47433358+JohnAkim10@users.noreply.github.com>
2022-09-13 15:34:41 +0100

    Update fonctions.php

--- fonctions.php
+++ fonctions.php
@@ -1,14 +1,11 @@
 <?php 
 
-	// $dbhost = 'localhost';
-	// $dbname = 'KINEXPRESS';
-	// $dbuser = 'root';
-	// $dbpswd = '';
+	 $dbhost = 'localhost';
+	$dbname = 'KINEXPRESS';
+	 $dbuser = 'root';
+	$dbpswd = '';
 
-  $dbhost = 'localhost';
-  $dbname = 'kimabouc_kimabouchon';
-  $dbuser = 'kimabouc_John12'; 
-  $dbpswd = 'authentique1234';
+
 
 	try{
 		$db = new PDO('mysql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpswd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
